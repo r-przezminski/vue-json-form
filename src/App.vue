db0b485447553d2ba77a95149bb59e6f6d5057eb
r-przezminski <r.przezminski@gmail.com>
2018-11-20 17:42:18 +0100

    Added base functionality

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-
+  <Form :schema="schema"></Form>
 </template>
 
 <script>
-export default {
+import FieldsCreator from '@/components/FieldsCreator';
+import schema from './schema';
 
+export default {
+  components: {
+    "Form": FieldsCreator,
+  },
+  data() {
+    return {
+      schema,
+    }
+  }
 }
 </script>
 
 <style>
-
+  * {
+    font-family: 'Lato', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: #837e87;
+  }
 </style>
-
